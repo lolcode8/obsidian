7eb362db5e0fad06c6cf751a252deba3a25b1103
Rahul <rahuliodev8@gmail.com>
2019-10-17 14:13:29 -0700

    Add helper to resolve absolute imports

--- App.tsx
+++ App.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import { createAppContainer } from "react-navigation";
 import { StyleSheet, View, Button } from "react-native";
 
-import { AppNavigator } from "./src/navigation";
+import { AppNavigator } from "./src/Navigation";
 import { getUserPlaylists } from "./src/services/spotify-auth/get-user-playlists";
 
 export default createAppContainer(AppNavigator);
