9f2fc5b40011bf644d7751bbc888bcef8c9292ff
Rahulio <rahuliodev8@gmail.com>
2020-03-25 18:40:10 -0400

    Updates

--- App.tsx
+++ App.tsx
@@ -1,16 +1,7 @@
-import React from "react";
 import { createAppContainer } from "react-navigation";
-import { StyleSheet, View, Button } from "react-native";
+import StorybookUIRoot from "./storybook";
 
 import { AppNavigator } from "./src/Navigation";
 
 export default createAppContainer(AppNavigator);
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center"
-  }
-});
+// export default StorybookUIRoot;
