6f30be619a24625836d00ca8200eaf3753fd980f
Rahulio <rahuliodev8@gmail.com>
2020-04-05 23:01:45 -0400

    Cleanup

--- App.tsx
+++ App.tsx
@@ -1,7 +1,5 @@
 import { createAppContainer } from "react-navigation";
-import StorybookUIRoot from "./storybook";
 
 import { AppNavigator } from "./src/Navigation";
 
 export default createAppContainer(AppNavigator);
-// export default StorybookUIRoot;
